422520572b7a7376c469094345c40e46f7450dd5
Jelmer Poelstra <poelstra.1@osu.edu>
2026-05-26 18:21:11 -0400

    Update to latest template

--- download/dl-blastdb.sh
+++ download/dl-blastdb.sh
@@ -4,15 +4,15 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
 #SBATCH --mail-type=END,FAIL
-#SBATCH --job-name=blastdb_download
-#SBATCH --output=slurm-blastdb_download-%j.out
+#SBATCH --job-name=dl-blastdb
+#SBATCH --output=slurm-dl-blastdb-%j.out
 
 # ==============================================================================
 #                          CONSTANTS AND DEFAULTS
 # ==============================================================================
 # Constants - generic
 DESCRIPTION="Download a standard NCBI BLAST database"
-SCRIPT_VERSION="2025-02-16"
+SCRIPT_VERSION="2026-05-26"
 SCRIPT_AUTHOR="Jelmer Poelstra"
 REPO_URL=https://github.com/mcic-osu/mcic-scripts
 FUNCTION_SCRIPT_URL=https://raw.githubusercontent.com/mcic-osu/mcic-scripts/main/dev/bash_functions.sh
@@ -23,44 +23,51 @@ VERSION_COMMAND="$TOOL_BINARY --version"
 
 # Defaults - generics
 env_type=conda                           # Use a 'conda' env or a Singularity 'container'
-conda_path=/fs/ess/PAS0471/conda/blast-2.16.0
+conda_path=/fs/ess/PAS0471/conda/blast-2.17.0
+container_url=oras://community.wave.seqera.io/library/blast:2.17.0--3d1eb1104ccfd59c
 container_dir="$HOME/containers"
+container_path=
 
 # ==============================================================================
 #                                   FUNCTIONS
 # ==============================================================================
 script_help() {
-    echo -e "\n                          $0"
-    echo "      (v. $SCRIPT_VERSION by $SCRIPT_AUTHOR, $REPO_URL)"
-    echo "        =============================================================="
-    echo "DESCRIPTION:"
-    echo "  $DESCRIPTION"
-    echo
-    echo "USAGE / EXAMPLE COMMANDS:"
-    echo "  - Basic usage example:"
-    echo "      sbatch $0 -db nt -o results/blastdb"
-    echo
-    echo "REQUIRED OPTIONS:"
-    echo "  --db                <file>  NCBI BLAST database to download"
-    echo "                              Possible options include: nr, nt, core_nt, nt_euk, nt_viruses, swissprot, taxdb, mito"
-    echo "                              Run 'update_blastdb.pl --showall' to see all options"
-    echo "  -o/--outdir         <dir>   Output dir for BLAST database (will be created if needed)"
-    echo
-    echo "OTHER KEY OPTIONS:"
-    echo "  --more_opts         <str>   Quoted string with additional options for $TOOL_NAME"
-    echo
-    echo "UTILITY OPTIONS:"
-    echo "  --env_type               <str>   Use a Singularity container ('container') or a Conda env ('conda') [default: $env_type]"
-    echo "                                (NOTE: If no default '--container_url' is listed below,"
-    echo "                                 you'll have to provide one in order to run the script with a container.)"
-    echo "  --conda_env         <dir>   Full path to a Conda environment to use [default: $conda_path]"
-    echo "  --container_url     <str>   URL to download the container from      [default: $container_url]"
-    echo "  --container_dir     <str>   Dir to download the container to        [default: $container_dir]"
-    echo "  -h/--help                   Print this help message and exit"
-    echo "  -v                          Print the version of this script and exit"
-    echo "  --version                   Print the version of $TOOL_NAME and exit"
-    echo
-    echo "TOOL DOCUMENTATION: $TOOL_DOCS"
+        echo -e "
+                                                $0
+        v. $SCRIPT_VERSION by $SCRIPT_AUTHOR, $REPO_URL
+                        =================================================
+
+DESCRIPTION:
+$DESCRIPTION
+
+USAGE / EXAMPLE COMMANDS:
+    - Basic usage example:
+            sbatch $0 --db nt -o results/blastdb
+
+REQUIRED OPTIONS:
+    --db                <str>   NCBI BLAST database to download
+                                Possible options include: nr, nt, core_nt, nt_euk,
+                                nt_viruses, swissprot, taxdb, mito
+                                Run 'update_blastdb.pl --showall' to see all options
+    -o/--outdir         <dir>   Output dir for BLAST database
+
+OTHER KEY OPTIONS:
+    --more_opts         <str>   Quoted string with one or more additional options
+                                for $TOOL_NAME
+
+UTILITY OPTIONS:
+    --env_type          <str>   Whether to use a Singularity/Apptainer container [default: $env_type]
+                                ('container') or a Conda environment ('conda')
+    --container_url     <str>   URL to download a container from                [default (if any): $container_url]
+    --container_dir     <str>   Dir to download a container to                  [default: $container_dir]
+    --container_path    <file>  Local container image file ('.sif') to use      [default (if any): $container_path]
+    --conda_path        <dir>   Full path to a Conda environment to use         [default (if any): $conda_path]
+    -h/--help                   Print this help message
+    -v/--version                Print script and $TOOL_NAME versions
+
+TOOL DOCUMENTATION:
+    $TOOL_DOCS
+"
 }
 
 # Function to source the script with Bash functions
@@ -75,23 +82,25 @@ source_function_script() {
         SCRIPT_NAME=$(basename "$0")
     fi
     function_script_name="$(basename "$FUNCTION_SCRIPT_URL")"
-    function_script="$script_dir"/../dev/"$function_script_name"
+    function_script_path="$script_dir"/../dev/"$function_script_name"
 
     # Download the function script if needed, then source it
-    if [[ -f "$function_script" ]]; then
-        source "$function_script"
-    elif [[ ! -f "$function_script_name" ]]; then
-        echo "Can't find script with Bash functions ($function_script_name), downloading from GitHub..."
-        wget -q "$FUNCTION_SCRIPT_URL" -O "$function_script_name"
-        source "$function_script_name"
+    if [[ -f "$function_script_path" ]]; then
+        # shellcheck source=/dev/null
+        source "$function_script_path"
     else
+        if [[ ! -f "$function_script_name" ]]; then
+            echo "Can't find script with Bash functions ($function_script_name), downloading from GitHub..."
+            wget -q "$FUNCTION_SCRIPT_URL" -O "$function_script_name"
+        fi
+        # shellcheck source=/dev/null
         source "$function_script_name"
     fi
 }
 
 # Check if this is a SLURM job, then load the Bash functions
 if [[ -z "$SLURM_JOB_ID" ]]; then IS_SLURM=false; else IS_SLURM=true; fi
-source_function_script
+source_function_script "$IS_SLURM"
 
 # ==============================================================================
 #                          PARSE COMMAND-LINE ARGS
@@ -101,9 +110,7 @@ db=
 outdir=
 more_opts=
 threads=
-container_path=
-container_url=
-version_only=false                 # When true, just print tool & script version info and exit
+version_only=false  # When true, just print tool & script version info and exit
 
 # Parse command-line args
 all_opts="$*"
@@ -112,11 +119,13 @@ while [ "$1" != "" ]; do
         --db )              shift && db=$1 ;;
         -o | --outdir )     shift && outdir=$1 ;;
         --more_opts )       shift && more_opts=$1 ;;
-        --env_type )             shift && env_type=$1 ;;
+        --env_type )        shift && env_type=$1 ;;
+        --conda_path )      shift && conda_path=$1 ;;
         --container_dir )   shift && container_dir=$1 ;;
         --container_url )   shift && container_url=$1 ;;
+        --container_path )  shift && container_path=$1 ;;
         -h | --help )       script_help; exit 0 ;;
-        -v | --version )         version_only=true ;;
+        -v | --version )    version_only=true ;;
         * )                 die "Invalid option $1" "$all_opts" ;;
     esac
     shift
@@ -129,15 +138,16 @@ done
 set -euo pipefail
 
 # Load software
-load_env "$conda_path" "$container_path"
+load_env "$env_type" "$conda_path" "$container_dir" "$container_path" "$container_url"
 [[ "$version_only" == true ]] && print_version "$VERSION_COMMAND" && exit 0
 
 # Check options provided to the script
-[[ -z "$db" ]] && die "No DB specified, do so with ---db" "$all_opts"
+[[ -z "$db" ]] && die "No DB specified, do so with --db" "$all_opts"
 [[ -z "$outdir" ]] && die "No output dir specified, do so with -o/--outdir" "$all_opts"
 
 # Define outputs based on script parameters
-LOG_DIR="$outdir"/logs && mkdir -p "$LOG_DIR"
+LOG_DIR="$outdir"/logs
+mkdir -p "$LOG_DIR"
 
 # ==============================================================================
 #                         REPORT PARSED OPTIONS
@@ -145,6 +155,8 @@ LOG_DIR="$outdir"/logs && mkdir -p "$LOG_DIR"
 log_time "Starting script $SCRIPT_NAME, version $SCRIPT_VERSION"
 echo "=========================================================================="
 echo "All options passed to this script:        $all_opts"
+echo "Working directory:                        $PWD"
+echo
 echo "Database to download:                     $db"
 echo "Output dir:                               $outdir"
 [[ -n $more_opts ]] && echo "Additional options for $TOOL_NAME:        $more_opts"
@@ -174,5 +186,5 @@ runstats $TOOL_BINARY \
 # ==============================================================================
 cd - || exit 1
 log_time "Listing files in the output dir:"
-ls -lh
+ls -lhd "$(realpath "$outdir")"/*
 final_reporting "$LOG_DIR"
